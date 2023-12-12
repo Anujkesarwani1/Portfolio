5d7b3e5cc25248dc70bff85b4ac2df1581f8cac5
Anuj Kesarwani <anuj.kesarwani@zemosolabs.com>
2023-12-12 21:55:42 +0530

    Navbar Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
+import styled, { ThemeProvider } from "styled-components";
+import "./App.css";
+import { darkTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Education from "./components/Education";
+import Skills from "./components/Skills";
+import { BrowserRouter } from "react-router-dom";
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%
+  overflow-x: hidden;
+`;
+
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 const App = () => {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar />
+        <Body>
+          <Home />
+          <Wrapper>
+            <Skills />
+            <Education />
+          </Wrapper>
+        </Body>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,117 @@
+import { DiCssdeck } from "react-icons/di";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { useTheme } from "styled-components";
+import {
+  ButtonContainer,
+  GitHubButton,
+  MobileIcon,
+  MobileMenuLink,
+  MobileMenu,
+  Nav,
+  NavContainer,
+  NavItems,
+  NavLink,
+  NavLogo,
+  Span,
+} from "./style";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const theme = useTheme();
+
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo to="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20;",
+              cursor: "pointer",
+            }}
+          >
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton>Github Profile</GitHubButton>
+        </ButtonContainer>
+      </NavContainer>
+      {isOpen && (
+        <MobileMenu isOpen={isOpen}>
+          <MobileMenuLink
+            href="#about"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            About
+          </MobileMenuLink>
+          <MobileMenuLink
+            href="#skills"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            Skills
+          </MobileMenuLink>
+          <MobileMenuLink
+            href="#experience"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            Experience
+          </MobileMenuLink>
+          <MobileMenuLink
+            href="#projects"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            Projects
+          </MobileMenuLink>
+          <MobileMenuLink
+            href="#education"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            Education
+          </MobileMenuLink>
+          <GitHubButton
+            style={{
+              padding: "10px 16px",
+              background: `${theme.primary}`,
+              color: "white",
+              width: "max-content",
+            }}
+            href="/"
+            target="_blank"
+          >
+            Github Profile
+          </GitHubButton>
+        </MobileMenu>
+      )}
+    </Nav>
+  );
+};
+
+export default Navbar;
